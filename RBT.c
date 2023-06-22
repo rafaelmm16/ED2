dfb4edb30dd99943d5ae0887fa017b3b1355268f
rafaelmm16 <rafael.merlomendes@gmail.com>
2023-06-22 17:25:12 -0300

    Foi corrigido o erro de rotações, porém a cor preta do nó ainda não esta sem aplicados aos demais nós.

--- RBT.c
+++ RBT.c
@@ -3,35 +3,154 @@
 #include "RBT.h"
 
 // Função auxiliar para criar um novo nó
-Node* createNode(int value)
+Node *createNode(int id)
 {
     Node *node = (Node *)malloc(sizeof(Node));
-    node->value = value;
-    node->color = 0;
+    node->id = id;
+    node->color = RED;
     node->left = NULL;
     node->right = NULL;
-    node->id = 0;
 
     return node;
 }
 
-Node *insert(Node *root, int value)
+Node *insert(Node *root, int id)
 {
-    if (root == NULL)
-        return createNode(value);
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->id = id;
+    node->color = RED; // Novos nós sempre são vermelhos
+    node->left = NULL;
+    node->right = NULL;
 
-    if (value < root->value)
-        root->left = insert(root->left, value);
-    else if (value > root->value)
-        root->right = insert(root->right, value);
+    // Caso base: árvore vazia
+    if (root == NULL)
+    {
+        root = node;
+        root->color = BLACK; // Raiz sempre é preta
+        return root;
+    }
+
+    insertNode(root, node);
+    fixupAfterInsertion(&root, node);
 
     return root;
 }
 
-void print(Node *root) {
-    if (root != NULL) {
+void insertNode(Node *currentNode, Node *node)
+{
+    if (node->id < currentNode->id)
+    {
+        if (currentNode->left == NULL)
+        {
+            currentNode->left = node;
+        }
+        else
+        {
+            insertNode(currentNode->left, node);
+        }
+    }
+    else
+    {
+        if (currentNode->right == NULL)
+        {
+            currentNode->right = node;
+        }
+        else
+        {
+            insertNode(currentNode->right, node);
+        }
+    }
+}
+
+void print(Node *root)
+{
+    if (root != NULL)
+    {
         print(root->left);
-        printf("%d ", root->value);
+        printf("%d ", root->id);
+        printf("%d ", root->color);
         print(root->right);
+    }
+}
+
+Node *rotateLeft(Node **root, Node *node)
+{
+    Node *rightChild = node->right;
+    node->right = rightChild->left;
+    rightChild->left = node;
+    if (*root == node)
+    {
+        *root = rightChild;
+    }
+
+    return rightChild;
+}
+
+Node *rotateRight(Node **root, Node *node)
+{
+    Node *leftChild = node->left;
+    node->left = leftChild->right;
+    leftChild->right = node;
+    if (*root == node)
+    {
+        *root = leftChild;
+    }
+
+    return leftChild;
+}
+
+Node *grandparent(Node *node)
+{
+    if (node != NULL && node->left != NULL)
+    {
+        return node->left->left;
+    }
+    return NULL;
+}
+
+Node *uncle(Node *node)
+{
+    Node *g = grandparent(node);
+    if (g == NULL)
+    {
+        return NULL;
+    }
+    if (node->left == g->right)
+    {
+        return g->left;
+    }
+    else
+    {
+        return g->right;
+    }
+}
+
+void fixupAfterInsertion(Node **root, Node *node)
+{
+    node->color = RED;
+
+    while (node != *root && node->left != NULL && node->left->color == RED)
+    {
+        Node *uncleNode = uncle(node);
+        Node *grandparentNode = grandparent(node);
+
+        if (uncleNode != NULL && uncleNode->color == RED)
+        {
+            node->color = BLACK;
+            node->left->color = BLACK;
+            grandparentNode->color = RED;
+            node = grandparentNode;
+        }
+        else
+        {
+            if (node->left == grandparentNode->right)
+            {
+                node = node->left;
+                rotateLeft(root, node);
+            }
+            node->left->color = BLACK;
+            grandparentNode->color = RED;
+            rotateRight(root, grandparentNode);
+        }
     }
 }
--- RBT.h
+++ RBT.h
@@ -1,18 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum Color
+{
+    RED,
+    BLACK
+} Color;
+
 // Estrutura de um nó da árvore
-typedef struct Node {
-    int value;
-    int color; // 0 para vermelho, 1 para preto
+typedef struct Node
+{
+    Color color; // 0 para vermelho, 1 para preto
     struct Node *left;
     struct Node *right;
     int id;
 } Node;
 
 // Função auxiliar para rotacionar a árvore à esquerda
-Node* createNode(int value);
+Node *createNode(int value);
 
 Node *insert(Node *root, int value);
 
+void insertNode(Node *currentNode, Node *node);
+
 void print(Node *root);
+
+Node *rotateLeft(Node **root, Node *node);
+
+Node *rotateRight(Node **root, Node *node);
+
+Node *grandparent(Node *node);
+
+Node *uncle(Node *node);
+
+void fixupAfterInsertion(Node **root, Node *newNode);
